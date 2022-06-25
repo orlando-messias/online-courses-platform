d04910cf5bf7541ed1969cf44f8229ccc576e950
orlando-messias <programadorlando@gmail.com>
2022-06-25 09:30:57 -0300

    adjusts the component Lesson to receive props

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,12 +7,12 @@ export function Sidebar() {
         Cronograma de Aulas
       </span>
       <div className="flex flex-col gap-8">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        <Lesson
+          title="Aula 01"
+          slug="aula-01"
+          availableAt={new Date()}
+          type="live"
+        />
       </div>
     </aside>
   );
